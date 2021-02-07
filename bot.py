00cf28827a39e8cd7414d312c905ba84c05c6a65
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2021-02-07 18:16:42 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 #bot token
 TOKEN = ""  # 봇 토큰 값
-json_file_name = '/'  # 구글에서 발급받은 키값
+json_file_name = ''  # 구글에서 발급받은 키값
 spreadsheet_url = ''  # 시트의 주소
 
 
@@ -71,40 +71,30 @@ async def on_ready():  # 봇이 실행 준비가 되었을 때 행동할 것
 
 @client.command(name='도움')
 async def roll(ctx):
-    await ctx.send('!영토전참가 or !영토전참가 [이름]\n!영토전불참 or !영토전불참 [이름]')
-
+    await ctx.send('!영토전참가 [이름]\n!영토전불참 [이름]')
 
 @client.command(name='영토전참가')
-async def roll(ctx):#뒤에 없을때
-    try:
-            user = ctx.author
-            Guild_member = worksheet.find(user.display_name)
-            worksheet.update_cell(Guild_member.row, 7, "O")
-            Now_member = worksheet.acell('G15').value
-            await ctx.send(f'"{user.display_name}" 영토전불참 확인됨 [참가인원] {Now_member}명')
-    except:
-            await ctx.send(f'이름이 없거나 틀림')
-
 async def roll(ctx,Inputname):#닉네임 지정해서 쓸때
     try:
         Guild_member = worksheet.find(Inputname)
         worksheet.update_cell(Guild_member.row, 7, "O")
         Now_member = worksheet.acell('G15').value
-        await ctx.send(f'"{Inputname}" 영토전불참 확인됨 [참가인원] {Now_member}명')
+        await ctx.send(f'"{Inputname}" 영토전참가 확인됨 [참가인원] {Now_member}명')
     except:
         await ctx.send(f'"{Inputname}" 이름이 없거나 틀림')
 
-@client.command(name='영토전불참')
-async def roll(ctx):
-    try:
-            user = ctx.author
-            Guild_member = worksheet.find(user.display_name)
-            worksheet.update_cell(Guild_member.row, 7, "X")
-            Now_member = worksheet.acell('G15').value
-            await ctx.send(f'"{user.display_name}" 영토전불참 확인됨 [참가인원] {Now_member}명')
-    except:
-            await ctx.send(f'이름이 없거나 틀림')
 
+# async def roll(ctx):  # 뒤에 없을때
+#     try:
+#         user = ctx.author
+#         Guild_member = worksheet.find(user.display_name)
+#         worksheet.update_cell(Guild_member.row, 7, "O")
+#         Now_member = worksheet.acell('G15').value
+#         await ctx.send(f'"{user.display_name}" 영토전참가 확인됨 [참가인원] {Now_member}명')
+#     except:
+#         await ctx.send(f'이름이 없거나 틀림')
+
+@client.command(name='영토전불참')
 async def roll(ctx,Inputname):
     try:
         Guild_member = worksheet.find(Inputname)
@@ -115,8 +105,8 @@ async def roll(ctx,Inputname):
         await ctx.send(f'"{Inputname}" 이름이 없거나 틀림')
 
 
-ok_hour = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-channel = 807086592266338305
+ok_hour = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]#  GMT 기준으로 측정되나봄
+channel = 807086592266338305 #채널코드
 msg = "♚영☆토☆전♚참가시$\n$전원 은화☜☜공헌도100%증정※\n ♜컨커러스 블레이드♜시즌미션 무료도움￥\n 특정조건 §§관녕철기병§§★칸케식★\n@@앙카디아 수복기회@@ \n$$고오급 전사로 승급할 절호의 기회$$\n즉시참가 영토전-참여여부 (!영토전참가)"
 loop = asyncio.get_event_loop
 
@@ -135,7 +125,7 @@ async def looping(ctx):
 async def alarm(ctx):
     if ctx.guild:
         if ctx.message.author.guild_permissions.manage_messages:
-            await ctx.send(f'지금부터 영토전 흥보를 시작함')
+            await ctx.send(f'지금부터 영토전 흥보를 시작함 13시-23시 1시간간격')
             client.loop.create_task(looping(ctx))
 
 client.run(TOKEN)
