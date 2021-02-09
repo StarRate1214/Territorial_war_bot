500fd04d506f2386669e83f9bc1f5cdfb9bce152
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2021-02-09 17:37:00 +0900

    Update bot.py
    시간변경 병종입력 에외처리

--- bot.py
+++ bot.py
@@ -67,9 +67,9 @@ async def on_ready():  # 봇이 실행 준비가 되었을 때 행동할 것
     await client.change_presence(status=discord.Status.online, activity=discord.Game('"!도움" 도움말 호출'))
 
 
-ok_hour = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]  # GMT 기준으로 측정되나봄
+ok_hour = [4, 6, 8, 10, 12, 14]  # GMT 기준으로 측정되나봄 +9시간
 channel = 807086592266338305
-msg = "♚영☆토☆전♚참가시$\n$전원 은화☜☜공헌도100%증정※\n ♜컨커러스 블레이드♜시즌미션 무료도움￥\n 특정조건 §§관녕철기병§§★칸케식★\n@@앙카디아 수복기회@@ \n$$고오급 전사로 승급할 절호의 기회$$\n즉시참가 영토전-참여여부 (!영토전참가)"
+msg = "♚영☆토☆전♚참가시$\n$전원 은화☜☜공헌도100%증정※\n♜컨커러스 블레이드♜시즌미션 무료도움￥\n특정조건 §§관녕철기병§§★칸케식★\n$$고오급 전사로 승급할 절호의 기회$$\n즉시참가 영토전-참여여부 (!영토전참가)"
 
 loop = asyncio.get_event_loop
 
@@ -80,35 +80,39 @@ async def looping(ctx):
     await client.get_channel(channel).send(msg)
     while True:
         if (old_hour != now.hour) & (now.hour in ok_hour):
-                    old_hour = now.hour
-                    now = datetime.datetime.now()
-                    await client.get_channel(channel).send(msg)
+            old_hour = now.hour
+            now = datetime.datetime.now()
+            await client.get_channel(channel).send(msg)
         await asyncio.sleep(60)
         now = datetime.datetime.now()
 
+
 @client.event
 async def on_message(ctx):
+    if ctx.author == client.user:
+        return
+
     if ctx.content.startswith("!병종입력"):
         try:
             user = ctx.author  # 디코이름 가져오기
             Guild_member = worksheet_army.find(user.display_name)  # 이름 찾기
-            pic1 = ctx.content.split(" ")[1]
-            worksheet_army.update_cell(Guild_member.row, 2, pic1)
-            pic2 = ctx.content.split(" ")[2]
-            worksheet_army.update_cell(Guild_member.row, 3, pic2)
-            pic3 = ctx.content.split(" ")[3]
-            worksheet_army.update_cell(Guild_member.row, 4, pic3)
+            pic = ctx.content[6:]
+            arr = pic.split("/")
+            col_num = 3
+            for i in arr:
+                worksheet_army.update_cell(Guild_member.row, col_num, i)
+                col_num = col_num + 1
             await ctx.channel.send(f'"{user.display_name}" 병종입력 성공')
         except:
             await ctx.channel.send(f'병종입력 실패')
 
-    if ctx.content.startswith ("!흥보시작"):
+    if ctx.content.startswith("!흥보시작"):
         if ctx.guild:
             if ctx.author.guild_permissions.manage_messages:
-                await ctx.channel.send(f'지금부터 영토전 흥보를 시작함 13시-23시 1시간간격')
+                await ctx.channel.send(f'지금부터 영토전 흥보를 시작함 13시-23시 2시간간격')
                 client.loop.create_task(looping(ctx))
 
-    if ctx.content.startswith ("!흥보메시지"):
+    if ctx.content.startswith("!흥보메시지"):
         if ctx.guild:
             if ctx.author.guild_permissions.manage_messages:
                 pic = ctx.content[7:]
@@ -116,14 +120,14 @@ async def on_message(ctx):
                 msg = pic
                 await ctx.channel.send(f'msg확인 "'+msg+'"')
 
-    if ctx.content.startswith ("!도움"):
-        await ctx.send('!영토전참가 or !영토전참가 [이름]\n!영토전불참 or !영토전불참 [이름]\n !병종입력 [병종1] [병종2] [병종3] | 영토전에 가져오는 병종입력')
+    if ctx.content.startswith("!도움"):
+        await ctx.channel.send('!영토전참가 or !영토전참가 [이름] |영토전 참가하기 둘 중 아무거나 사용가능\n!영토전불참 or !영토전불참 [이름] | 영토전 불참하기 둘 중 아무거나 사용가능\n !병종입력 [병종1]/[병종2]/[병종3] | 영토전에 가져오는 병종입력 최대 5')
 
-    if ctx.content.startswith ("!서버도움"):
-        await ctx.send('!흥보시작 | 13시-23시 1시간간격 메시지 보냄\n!흥보메시지 [메시지] | 흥보문구 변경가능 아마도...')
+    if ctx.content.startswith("!서버도움"):
+        await ctx.channel.send('!흥보시작 | 13시-23시 2시간간격 메시지 보냄\n!흥보메시지 [메시지] | 흥보문구 변경')
 
     #print ('msg:'+message.content)
-    if ctx.content.startswith ("!영토전참가"):
+    if ctx.content.startswith("!영토전참가"):
         pic = ctx.content[7:]
         #print('pic"'+pic+'"')
         if pic != '':
@@ -145,7 +149,7 @@ async def on_message(ctx):
                 await ctx.channel.send(f'이름이 없거나 틀림')
 
     #print ('msg:'+message.content)
-    if ctx.content.startswith ("!영토전불참"):
+    if ctx.content.startswith("!영토전불참"):
         pic = ctx.content[7:]
         #print('pic"'+pic+'"')
         if pic != '':
@@ -165,7 +169,7 @@ async def on_message(ctx):
                 await ctx.channel.send(f'"{user.display_name}" 영토전불참 확인됨 [참가인원] {Now_member}명')
             except:
                 await ctx.channel.send(f'이름이 없거나 틀림')
-        
+
 
 # @client.command(name='영토전참가')
 # async def roll(ctx,Inputname):#닉네임 지정해서 쓸때
